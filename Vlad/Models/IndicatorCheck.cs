ecc88f716be54a5b164e1c7e69f62554a3c4f3dc
Отчик Влад <79385087+OtchykVladzislav@users.noreply.github.com>
2021-11-18 18:58:17 +0300

    описаны классы models

---
+++ Vlad/Models/IndicatorCheck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Vlad.Models
+{
+    public class IndicatorCheck
+    {
+        [DisplayName("ID")]
+        public int Id { get; set; }
+        [DisplayName("ID показания")]
+        public int IndicatorId { get; set; }
+        [DisplayName("Дата")]
+        public DateTime Date { get; set; }
+        [DisplayName("Информация")]
+        public string Info { get; set; }
+    }
+}
