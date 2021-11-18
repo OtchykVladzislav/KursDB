9cc3d3462591db551a224bc58918a8f497537e37
Отчик Влад <79385087+OtchykVladzislav@users.noreply.github.com>
2021-11-18 20:16:04 +0300

    пересоздание проекта. Добавление Models и Context

--- Vlad/T-Sql/CreateCountesView.sql
+++
@@ -1,6 +0,0 @@
-USE VladDb
-GO
-CREATE VIEW CounterDateFilterView AS
-SELECT *
-FROM [dbo].[Counters] WHERE [dbo].[Counters].[CounterSetupDate] = GETDATE()
-	   
---
+++ WaterSupply/Models/WaterSupplyContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WaterSupply.Models
+{
+    public class WaterSupplyContext : DbContext
+    {
+        public DbSet<Organization> Organizations { get; set; }
+        public DbSet<CounterMark> CounterMarks { get; set; }
+        public DbSet<Counter> Counters { get; set; }
+        public DbSet<Indicator> Indicators { get; set; }
+        public DbSet<IndicatorCheck> IndicatorChecks { get; set; }
+
+        public WaterSupplyContext(DbContextOptions<WaterSupplyContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
