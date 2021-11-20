b92852dce687964f2390e29700e53d7288ed06fd
Отчик Влад <79385087+OtchykVladzislav@users.noreply.github.com>
2021-11-20 20:56:00 +0300

    Startup и Layout

---
+++ WaterSupply/Models/ExpenseVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace WaterSupply.Models
+{
+    public class ExpenseVM
+    {
+        [DisplayName("Организация")]
+        public string Org { get; set; }
+        [DisplayName("Среднемесячный расход")]
+        public decimal Value { get; set; }
+    }
+}
---
+++ WaterSupply/Models/PaymentVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace WaterSupply.Models
+{
+    public class PaymentVM
+    {
+        [DisplayName("Организация")]
+        public string OrgName { get; set; }
+        [DisplayName("Сумма")]
+        public decimal Value { get; set; }
+    }
+}
